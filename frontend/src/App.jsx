09c3c474c8ade21d09445cc012a4d105233dd987
sen98ia <sen98ia@gmail.com>
2025-01-09 21:16:20 +0300

    update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,6 @@
 // import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
-// import Container from 'react-bootstrap/Container';
-// import Card from 'react-bootstrap/Card';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
