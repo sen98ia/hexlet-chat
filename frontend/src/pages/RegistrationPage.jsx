7789f57f46a1036ad2f41f8cfa8e42089a4a8966
sen98ia <sen98ia@gmail.com>
2025-01-11 22:35:04 +0300

    rename RegistrationPage

---
+++ frontend/src/pages/RegistrationPage.jsx
@@ -0,0 +1,10 @@
+import PageLayout from "../components/PageLayout.jsx";
+import RegistrationCard from "../components/RegistrationCard.jsx";
+
+const RegistrationPage = () => {
+  return (
+    <PageLayout Component={RegistrationCard}></PageLayout>
+  );
+}
+
+export default RegistrationPage;
