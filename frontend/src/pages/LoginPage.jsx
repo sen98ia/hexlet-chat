8fd5188a3e22564b85636654a870eb2508fc53f9
sen98ia <sen98ia@gmail.com>
2025-01-11 22:34:21 +0300

    rename LoginPage

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+import PageLayout from "../components/PageLayout.jsx";
+import LoginCard from "../components/LoginCard.jsx";
+
+const LoginPage = () => {
+  return (
+    <PageLayout Component={LoginCard}></PageLayout>
+  );
+}
+
+export default LoginPage;
