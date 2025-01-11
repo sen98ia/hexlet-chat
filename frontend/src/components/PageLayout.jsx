c5502d27b3c48f81d3dd2fd5d03111cb94a4b9e0
sen98ia <sen98ia@gmail.com>
2025-01-11 22:35:58 +0300

    add PageLayout

---
+++ frontend/src/components/PageLayout.jsx
@@ -0,0 +1,21 @@
+import Container from "react-bootstrap/esm/Container";
+import Header from "./Header.jsx";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const PageLayout = ({Component}) => {
+  return (
+    <Container fluid className="h-100 d-flex flex-column">
+      <Row>
+        <Header/>
+      </Row>
+      <Row className="h-100">
+        <Col className="d-flex justify-content-center align-items-center">
+          <Component/>
+        </Col>
+      </Row>
+    </Container>   
+  );
+};
+
+export default PageLayout;
