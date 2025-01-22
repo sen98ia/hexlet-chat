d56dbd037b51fe521774a4cd6c16b9a4396084ee
sen98ia <sen98ia@gmail.com>
2025-01-22 22:34:10 +0300

    add fuction component arrow rule

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -41,6 +41,7 @@ export default [
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
+      'react/function-component-definition': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
