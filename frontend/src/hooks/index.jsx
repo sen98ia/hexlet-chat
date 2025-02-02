5bd1097a555bdfd34dcfbc78f4b79325b6d58a63
sen98ia <sen98ia@gmail.com>
2025-02-02 22:18:20 +0300

    update auth

--- frontend/src/hooks/index.jsx
+++ frontend/src/hooks/index.jsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 
-import authContext from '../contexts/index.jsx';
+import AuthContext from '../contexts/index.jsx';
 
-const useAuth = () => useContext(authContext);
+const useAuth = () => useContext(AuthContext);
 
 export default useAuth;
