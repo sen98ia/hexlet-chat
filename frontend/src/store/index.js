03b77a69e9aa25967f7ac41061d4cd2fcf5bee96
sen98ia <sen98ia@gmail.com>
2025-02-03 22:38:10 +0300

    add store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,1 +1,10 @@
-// тут буду делать configureStore
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './authSlice.js';
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
+
+export default store;
