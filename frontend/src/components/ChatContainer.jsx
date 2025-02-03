1fbd4441840e105bc104e1fabab1b734c809b888
sen98ia <sen98ia@gmail.com>
2025-02-03 22:36:46 +0300

    update chat container

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,16 +1,17 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import ChannelsPanel from './ChannelsPanel.jsx';
 
-function RegistrationCard() {
-  return (
-    <Container className="h-100 my-4 overflow-hidden shadow rounded">
-      <Row className="h-100 bg-white flex-md-row">
-        <Col>channels</Col>
-        <Col>chat</Col>
-      </Row>
-    </Container>
-  );
-}
+const ChatContainer = () => (
+  <Container className="h-100 my-4 overflow-hidden shadow rounded">
+    <Row className="h-100 bg-white flex-md-row">
+      <Col>
+        <ChannelsPanel />
+      </Col>
+      <Col>chat</Col>
+    </Row>
+  </Container>
+);
 
-export default RegistrationCard;
+export default ChatContainer;
