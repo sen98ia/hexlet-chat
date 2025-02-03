d35598afbdc460d5d7ebff500792c304feaefe20
sen98ia <sen98ia@gmail.com>
2025-02-03 22:35:16 +0300

    add auth and private route

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,25 +1,43 @@
-// import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../store/index.js';
+import AuthProvider from '../contexts/AuthProvider.jsx';
+import useAuth from '../hooks/index.jsx';
 import LoginPage from '../pages/LoginPage.jsx';
 import RegistrationPage from '../pages/RegistrationPage.jsx';
 import NotFoundPage from '../pages/NotFoundPage.jsx';
-// import ChatPage from './pages/ChatPage';
+import ChatPage from '../pages/ChatPage.jsx';
 
-function App() {
+const PrivateRoute = ({ children }) => {
+  const auth = useAuth();
+  const location = useLocation();
+
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* <PrivateRoute>
-          <Route path="/" element={<ChatPage />} />
-        </PrivateRoute> */}
-        {/* <Route path="/" element={<LoginPage />} /> */}
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/registration" element={<RegistrationPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    auth.isAuth ? children : <Navigate to="/login" state={{ from: location }} />
   );
-}
+};
+
+const App = () => (
+  <Provider store={store}>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ChatPage />} />
+          <Route path="/" element={<PrivateRoute><ChatPage /></PrivateRoute>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/registration" element={<RegistrationPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  </Provider>
+);
 
 export default App;
