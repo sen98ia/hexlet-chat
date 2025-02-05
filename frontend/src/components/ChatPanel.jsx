a3c460787f6b82df314ce5d663f0d92df9051ea2
sen98ia <sen98ia@gmail.com>
2025-02-05 18:21:09 +0300

    update ChatPanel

--- frontend/src/components/ChatPanel.jsx
+++ frontend/src/components/ChatPanel.jsx
@@ -1,2 +1,25 @@
-// шапка с тенью
-// контейнер, в котором лежит контейнер для сообщений и форма отправки
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+const ChatPannel = () => (
+  <Container>
+    <Row>
+      <Container>
+        <Row>
+          <b>имя канала</b>
+        </Row>
+        <Row>
+          <span>количество сообщений</span>
+        </Row>
+      </Container>
+    </Row>
+    <Row>
+      для сообщений
+    </Row>
+    <Row>
+      форма для отправки сообщений
+    </Row>
+  </Container>
+);
+
+export default ChatPannel;
