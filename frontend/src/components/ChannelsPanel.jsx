8ed1fd10e508a8682b0caea8777dbe64029740c1
sen98ia <sen98ia@gmail.com>
2025-02-05 18:20:20 +0300

    update ChannelsPanel

--- frontend/src/components/ChannelsPanel.jsx
+++ frontend/src/components/ChannelsPanel.jsx
@@ -3,21 +3,28 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
+import { getChannels } from '../store/api/channelsApi.js';
 
 const ChannelsPanel = () => {
-  const lala = 0;
+  const { data: channels, isLoading, refetch } = getChannels();
+
+  if (isLoading) {
+    return <div>жду</div>;
+  }
 
   return (
-    <Container>
-      <Row className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
+    <Container className="border-end">
+      <Row>
         <Col><b>Каналы</b></Col>
         <Col><Button variant="outline-primary" size="sm">+</Button></Col>
       </Row>
       <Row>
         <Nav defaultActiveKey="/home" className="flex-column">
-          <Nav.Link href="/home">Active</Nav.Link>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
+          {channels.map((channel) => (
+            <Nav.Item key={channel.id}>
+              <Nav.Link eventKey={channel.id}>{channel.name}</Nav.Link>
+            </Nav.Item>
+          ))}
         </Nav>
       </Row>
     </Container>
