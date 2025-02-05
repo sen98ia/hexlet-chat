ec4df12138a62aa151d17321923d759d9f790e94
sen98ia <sen98ia@gmail.com>
2025-02-05 18:20:51 +0300

    update ChatContainer

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -2,14 +2,17 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ChannelsPanel from './ChannelsPanel.jsx';
+import ChatPannel from './ChatPanel.jsx';
 
 const ChatContainer = () => (
-  <Container className="h-100 my-4 overflow-hidden shadow rounded">
-    <Row className="h-100 bg-white flex-md-row">
-      <Col>
+  <Container>
+    <Row>
+      <Col md={2}>
         <ChannelsPanel />
       </Col>
-      <Col>chat</Col>
+      <Col>
+        <ChatPannel />
+      </Col>
     </Row>
   </Container>
 );
