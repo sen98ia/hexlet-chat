0c108c083ddcec483b9827a9f44d137018cdea7f
sen98ia <sen98ia@gmail.com>
2025-02-05 18:22:08 +0300

    update store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice.js';
+import { channelsApi } from './api/channelsApi.js';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    [channelsApi.reducerPath]: channelsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(channelsApi.middleware),
 });
 
 export default store;
