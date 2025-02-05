a2e41375d11c34767996830ec6f4c9809a5991e5
sen98ia <sen98ia@gmail.com>
2025-02-05 23:03:53 +0300

    fix header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary shadow-sm">
       <Container>
-        <Navbar.Brand href="/">Hextlet Chat</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Hextlet Chat</Navbar.Brand>
       </Container>
     </Navbar>
   );
