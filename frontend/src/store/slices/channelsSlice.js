0be81bbd1ae0af921ece318ceaa016e64d4270c4
sen98ia <sen98ia@gmail.com>
2025-02-12 22:05:58 +0300

    add setDefault

--- frontend/src/store/slices/channelsSlice.js
+++ frontend/src/store/slices/channelsSlice.js
@@ -8,9 +8,12 @@ const channelsSlice = createSlice({
       const channel = payload;
       Object.assign(state, { activeChannel: channel });
     },
+    setDefault: (state) => {
+      Object.assign(state, { activeChannel: { id: '1', name: 'general', removable: false } });
+    },
   },
 });
 
-export const { setActive } = channelsSlice.actions;
+export const { setActive, setDefault } = channelsSlice.actions;
 export const selectors = channelsSlice.getSelectors((state) => state.channels);
 export default channelsSlice.reducer;
