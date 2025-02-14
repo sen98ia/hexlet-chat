9de9ec463ab9e1c7518d82548d188d0488ffcbda
sen98ia <sen98ia@gmail.com>
2025-02-14 16:42:35 +0300

    update routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,4 +4,6 @@ export default {
   loginPath: () => [apiPath, 'login'].join('/'),
   channelsPath: () => [apiPath, 'channels'].join('/'),
   messagesPath: () => [apiPath, 'messages'].join('/'),
+  signUpPath: () => [apiPath, 'signup'].join('/'),
+  apiPath: () => apiPath,
 };
