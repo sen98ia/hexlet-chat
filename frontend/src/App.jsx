630e2f77dd48df7d4c30e3b029ecf10ba4a3bab7
sen98ia <sen98ia@gmail.com>
2025-02-18 12:48:32 +0300

    fix error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,13 @@ import {
   Navigate,
   useLocation,
 } from 'react-router-dom';
+import { Provider as StoreProvider } from 'react-redux';
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { Provider, ErrorBoundary } from '@rollbar/react';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { ToastContainer, Bounce } from 'react-toastify';
 import filter from 'leo-profanity';
+import store from './store/index.js';
 // import store from './store/index.js';
 import AuthProvider from './contexts/AuthProvider.jsx';
 import useAuth from './hooks/index.jsx';
@@ -19,13 +21,14 @@ import RegistrationPage from './pages/RegistrationPage.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
 import ChatPage from './pages/ChatPage.jsx';
 import resources from './locales/index.js';
+import routes from './routes.js';
 
 const PrivateRoute = ({ children }) => {
   const auth = useAuth();
   const location = useLocation();
 
   return (
-    auth.isAuth ? children : <Navigate to="/login" state={{ from: location }} />
+    auth.isAuth ? children : <Navigate to={routes.pages.login()} state={{ from: location }} />
   );
 };
 
@@ -42,43 +45,50 @@ const App = () => {
       },
       debug: false,
     });
-
+  // leo-profanity
   const ru = filter.getDictionary('ru');
   filter.add(ru);
-
+  // rollbar-config
   const rollbarConfig = {
-    accessToken: '*****',
+    accessToken: import.meta.env.ROLLBAR_ACCESS_TOKEN,
     captureUncaught: true,
     captureUnhandledRejections: true,
     environment: 'production',
   };
 
   return (
-    <Provider config={rollbarConfig}>
-      <AuthProvider>
-        <BrowserRouter>
-          <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick={false}
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-            transition={Bounce}
-          />
-          <Routes>
-            <Route path="/" element={<PrivateRoute><ChatPage /></PrivateRoute>} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<RegistrationPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
-    </Provider>
+    <StoreProvider store={store}>
+      <RollbarProvider config={rollbarConfig}>
+        <ErrorBoundary>
+          <AuthProvider>
+            <BrowserRouter>
+              <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                transition={Bounce}
+              />
+              <Routes>
+                <Route
+                  path={routes.pages.chat()}
+                  element={<PrivateRoute><ChatPage /></PrivateRoute>}
+                />
+                <Route path={routes.pages.login()} element={<LoginPage />} />
+                <Route path={routes.pages.signUp()} element={<RegistrationPage />} />
+                <Route path={routes.pages.notFound()} element={<NotFoundPage />} />
+              </Routes>
+            </BrowserRouter>
+          </AuthProvider>
+        </ErrorBoundary>
+      </RollbarProvider>
+    </StoreProvider>
   );
 };
 
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,4 @@
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './App.jsx';
-import store from './store/index.js';
 
-createRoot(document.getElementById('root')).render(<Provider store={store}><App /></Provider>);
+createRoot(document.getElementById('root')).render(<App />);
